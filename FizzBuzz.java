5480a696d5f0577e85a17d0f15f4ad5be04ec9b6
ashwinidotx <ashwinix@icloud.com>
2018-09-01 20:29:59 +0530

    FizzBuzz Bugs

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -16,7 +16,9 @@ public class FizzBuzz
         range=sc.nextInt();
         for(int i=1;i<=range;i++)
         {
-            if(i%3==0)
+            if(i%3==0 && i%5==0)
+                System.out.println("FizzBuzz");
+            else if(i%3==0)
                 System.out.println("Fizz");
             else if(i%5==0)
                 System.out.println("Buzz");
